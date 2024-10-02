49ba96400be1bbe8f0e0c9fe0b917135532fbd07
editorweb <jorge.r@msn.com>
2024-10-02 10:57:54 -0500

    test

--- mision2/proyecto1/js/app.js
+++ mision2/proyecto1/js/app.js
@@ -1,18 +1,3 @@
-console.log("hola mundo");
-
-
-sessionStorage.setItem("key", "value");
-
-// Get saved data from sessionStorage
-let data = sessionStorage.getItem("key");
-
-
-console.log(data);
-
-
-localStorage.setItem("myCat", "Tom");
-
-
-const cat = localStorage.getItem("myCat");
-
-console.log(cat);
+for (let i=0; i<8; i+2){
+    console.log("numero" + i);
+}

822bc44584625dc2c2d0740b0fc0546c9908aea2
editorweb <jorge.r@msn.com>
2024-10-03 07:07:13 -0500

    test

---
+++ datos.js
@@ -0,0 +1,5 @@
+ estudiantes = [{nombre:"carlos", edad:40},
+    {nombre:"pedro", apellido:"perez", edad:25, },
+    {nombre:"juan", edad:33},
+    {nombre:"julio", edad:48},
+    {nombre:"Jose", edad:22}]
---
+++ mision2/proyecto2/js/app.js
@@ -0,0 +1,29 @@
+let edad = 18;
+if (edad < 18) {
+console.log("Eres menor de edad.");
+} else if (edad >= 18 && edad < 65) {
+console.log("Eres adulto.");
+} else {
+console.log("Eres mayor.");
+}
+
+
+// let dia = 3;
+// switch (dia) {
+// case 1:
+// console.log("Lunes");
+// break;
+// case 2:
+// console.log("Martes");
+// break;
+// case 3:
+// console.log("Miércoles");
+// break;
+// default:
+// console.log("Día no válido");
+// }
+
+
+// let edad = 20;
+// let mensaje = (edad >= 18) ? "Eres mayor de edad." : "Eres menor de edad.";
+// console.log(mensaje);

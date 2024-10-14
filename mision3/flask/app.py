3e7d5323f376ff29b956d77785472ffb933f08a5
editorweb <jorge.r@msn.com>
2024-10-14 12:04:24 -0500

    agrego flask

---
+++ mision3/flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Crear la aplicación de Flask
+app = Flask(__name__)
+
+
+# Definir una ruta básica
+@app.route("/")
+def home():
+    mensaje = "Bienvenido a mi aplicación con Flask"
+    return render_template("index.html", mensaje=mensaje)
+
+# Ejecutar la aplicación
+if __name__ == "__main__":
+    app.run(debug=True)
+

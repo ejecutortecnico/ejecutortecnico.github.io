ce66e4daee333a650532de16c5848212c5cbefd0
ejecutortecnico <ejecutortecnico@gmail.com>
2024-11-20 07:53:54 -0500

    test

--- mision2/todolist/js/app.js
+++ mision2/todolist/js/app.js
@@ -2,6 +2,7 @@ const input = document.getElementById("tarea");
 const boton = document.getElementById("agregar");
 const lista = document.getElementById("lista");
 const vacio = document.querySelector(".empty");
+tareas = []
 form = document.querySelector("form").addEventListener("submit",function(event){
     event.preventDefault();
 });
@@ -13,14 +14,23 @@ function jugar(){
         vacio.style.display = "none";
         element.innerText = input.value;
         lista.appendChild(element);
+        tareas.push(input.value)
+        localStorage.setItem("tareas", JSON.stringify(tareas))
     }else{
         alert("la tarea no puede estar vacia");
     }
     
 }
 
-usuario = {
-    nombre:"nombre",
-    apellido:"apellido",
-    password:"22342342"
+function cargar(){
+    tareas = JSON.parse(localStorage.getItem("tareas"))
+    vacio.style.display = "none";
+    for(tarea of tareas){
+        const element = document.createElement("li");
+        element.innerText = tarea;
+        lista.appendChild(element);
+
+    }
+    
+
 }

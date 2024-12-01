b0677362a33200a4069c41a1ec55e2e18aff3d9e
ejecutortecnico <ejecutortecnico@gmail.com>
2024-12-01 16:12:23 -0500

    test

--- mision3/datos/pruebapandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-labels = ["a","b","c","d","e","f","g","h","i","j"]
-lista = [1,2,3,4,5,6,7,8,9,10]
-print(lista)
-# Crear una Serie
-mi_serie = pd.Series(lista,labels)
-print(mi_serie)
